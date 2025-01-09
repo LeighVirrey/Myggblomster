051f523c8d7739e8e7cb49092df480f5d9a90215
Astokes2604 <129238930+Astokes2604@users.noreply.github.com>
2025-01-09 14:24:48 -0700

    app.js is setup for the basics of getting the app to run on the localhost

--- back-end/app.js
+++ back-end/app.js
@@ -1,1 +1,22 @@
-//ALEX DO THE STUFF
+// Import Express, Bcrypt(for password hashing), Cors(for cross-origin requests), Body Parser(for parsing JSON data)
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Intialize the app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+// Add app.get functions below
+
+// Add app.port functions below
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:{PORT}`);
+});
