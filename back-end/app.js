51ca7aba75122ec5ff60631d5d7b43b2097362d3
Astokes2604 <129238930+Astokes2604@users.noreply.github.com>
2025-01-10 10:39:39 -0700

    added a comment

--- back-end/app.js
+++ back-end/app.js
@@ -16,6 +16,8 @@ app.use(express.json());
 
 // Add app.port functions below
 
+
+// Sets local host to port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:{PORT}`);
