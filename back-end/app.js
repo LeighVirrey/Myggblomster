fd898dad24b02d1a0c4a46dd19a499a842d82911
TOONZK <126704941+LeighVirrey@users.noreply.github.com>
2025-01-13 12:15:02 -0700

    added review and users start
    doesn't have dal functionality yet

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const profanity = require('@2toad/profanity');
 
 // Intialize the app
 const app = express();
@@ -17,7 +18,36 @@ const { DAL } = require('./DAL/mongo-dal')
 // Add app.get functions below
 
 // Add app.port functions below
+//reviews and ratings
+app.post('/rarCreate', (req, res) => {
+    movieID = req.body.movieID;
+    rating = req.body.rating;
+    review = profanity.censor(req.body.review);
 
+    //DAL function to add review and rating
+    res.json({message: "Review and rating added successfully", rating: rating, review: review});
+});
+
+app.get('/rar/:id', (req, res) => {
+    id = req.params.id;
+    //DAL function to get review and rating
+    ratingAndReviews = [];
+    res.json({ratingAndReviews: ratingAndReviews});
+});
+
+app.delete('/rarDelete/:id', (req, res) => {
+    id = req.params.id;
+    //DAL function to delete review and rating
+    res.json({message: "Review and rating deleted successfully"});
+});
+
+app.put('/rarUpdate/:id', (req, res) => {
+    id = req.params.id;
+    rating = req.body.rating;
+    review = profanity.censor(req.body.review);
+    //DAL function to update review and rating
+    res.json({message: "Review and rating updated successfully", rating: rating, review: review});
+});
 
 // Sets local host to port 9000
 const PORT = process.env.PORT || 9000;
