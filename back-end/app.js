018a1af82ee03c6c0a6a607ea2b7b133fecf0ea7
TOONZK <126704941+LeighVirrey@users.noreply.github.com>
2025-01-13 08:53:38 -0700

    reviews and rating start
    dont have dal yet

--- back-end/app.js
+++ back-end/app.js
@@ -14,8 +14,39 @@ app.use(express.json());
 
 // Add app.get functions below
 
-// Add app.port functions below
 
+
+
+
+
+//reviews and ratings
+app.post('reviews-and-rating', (req, res) => {
+    rating = req.body.rating;
+    review = req.body.review;
+    //DAL function to add review and rating
+    res.json({message: "Review and rating added successfully", rating: rating, review: review});
+});
+
+app.get('reviews-and-rating/:id', (req, res) => {
+    id = req.params.id;
+    //DAL function to get review and rating
+    ratingAndReviews = [];
+    res.json({ratingAndReviews: ratingAndReviews});
+});
+
+app.delete('reviews-and-rating/:id', (req, res) => {
+    id = req.params.id;
+    //DAL function to delete review and rating
+    res.json({message: "Review and rating deleted successfully"});
+});
+
+app.put('reviews-and-rating/:id', (req, res) => {
+    id = req.params.id;
+    rating = req.body.rating;
+    review = req.body.review;
+    //DAL function to update review and rating
+    res.json({message: "Review and rating updated successfully", rating: rating, review: review});
+});
 
 // Sets local host to port 3000
 const PORT = process.env.PORT || 3000;
