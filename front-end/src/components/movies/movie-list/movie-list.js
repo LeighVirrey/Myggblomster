e9932a91f5a65efd241a1f4adf95f1d2ed18c277
Astokes2604 <129238930+Astokes2604@users.noreply.github.com>
2025-03-05 20:46:15 -0700

    updated movie search
    took total between last week and this week 2.25 hours

--- front-end/src/components/movies/movie-list/movie-list.js
+++ front-end/src/components/movies/movie-list/movie-list.js
@@ -4,6 +4,8 @@ import NavBar from '../../navbar/navbar';
 const MovieList = () => {
     const [movies, setMovies] = useState([]);
     const [searchQuery, setSearchQuery] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState([]);
     const API_KEY = '80ff9aff7ec44bb8644c249abba9fc74';
 
     useEffect(() => {
@@ -12,17 +14,25 @@ const MovieList = () => {
 
     const fetchMovies = async (query = "") => {
         let url = query
-            ? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`
+            ? `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`
             : `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
 
         try {
+            setLoading(true);
             const res = await fetch(url);
             const data = await res.json();
             document.body.style.backgroundImage = `./public/images/theaterbackground.avif`;
-            setMovies(data.results || []);
 
+            // Handle search results based on query
+            if (query) {
+                setSearchResults(data.results || []);
+            } else {
+                setMovies(data.results || []);
+            }
         } catch (err) {
             console.error(err);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -31,23 +41,54 @@ const MovieList = () => {
         fetchMovies(searchQuery);
     };
 
-    const redirectDetails = (id) => {
-        window.location.href = `/movies/${id}`;
+    const redirectDetails = (id, type) => {
+        window.location.href = `/movies/${id}/${type}`;
+    };
+
+    const renderSearchResults = (results) => {
+        return results.map(item => (
+            <li key={item.id} className="movie-item" onClick={() => redirectDetails(item.id, item.media_type)}>
+                {item.media_type === "movie" ? (
+                    <div className="movie-info">
+                        <img
+                            src={`https://image.tmdb.org/t/p/w185/${item.poster_path}`}
+                            alt={item.title || item.name}
+                            className="movie-poster"
+                        />
+                        <h2 className="movie-title">{item.title || item.name}</h2>
+                        <p className="movie-overview">{item.overview}</p>
+                    </div>
+                ) : item.media_type === "person" ? (
+                    <div className="actor-info">
+                        <img
+                            src={`https://image.tmdb.org/t/p/w185/${item.profile_path}`}
+                            alt={item.name}
+                            className="actor-image"
+                        />
+                        <h2>{item.name}</h2>
+                        <p>{item.known_for_department}</p>
+                        <button onClick={() => redirectDetails(item.id, 'person')}>View Movies</button>
+                    </div>
+                ) : item.media_type === "tv" ? (
+                    <div className="tv-show-info">
+                        <h2>{item.name}</h2>
+                        <p>{item.overview}</p>
+                    </div>
+                ) : null}
+            </li>
+        ));
     };
 
     return (
         <div>
             <NavBar />
-
-
-
             <div className="movie-container">
                 <h1 className="title bungee-shade-regular">Movies</h1>
                 <div className="search-container">
                     <form onSubmit={handleSearch}>
                         <input
                             type="text"
-                            placeholder="Search for a movie..."
+                            placeholder="Search for a movie, actor, or director..."
                             value={searchQuery}
                             onChange={(e) => setSearchQuery(e.target.value)}
                             className="search-input"
@@ -55,23 +96,33 @@ const MovieList = () => {
                         <button type="submit" className="search-button">Search</button>
                     </form>
                 </div>
+
+                <h2 className="search-results-title">Search Results</h2>
                 <ul className="movie-list">
-                    {movies.length > 0 ? (
-                        movies.map(movie => (
-                            <li key={movie.id} className="movie-item" onClick={() => redirectDetails(movie.id)}>
-                                <img
-                                    src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`}
-                                    alt={movie.title}
-                                    className="movie-poster"
-                                />
-                                <div className="movie-info">
-                                    <h2 className="movie-title">{movie.original_title}</h2>
-                                    <p className="movie-overview">{movie.overview}</p>
-                                </div>
-                            </li>
-                        ))
+                    {loading ? (
+                        <p>Loading...</p>
                     ) : (
-                        <p className="loading">No movies found...</p>
+                        searchQuery ? (
+                            renderSearchResults(searchResults)
+                        ) : (
+                            movies.length > 0 ? (
+                                movies.map(movie => (
+                                    <li key={movie.id} className="movie-item" onClick={() => redirectDetails(movie.id, "movie")}>
+                                        <img
+                                            src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`}
+                                            alt={movie.title}
+                                            className="movie-poster"
+                                        />
+                                        <div className="movie-info">
+                                            <h2 className="movie-title">{movie.original_title}</h2>
+                                            <p className="movie-overview">{movie.overview}</p>
+                                        </div>
+                                    </li>
+                                ))
+                            ) : (
+                                <p>No movies found...</p>
+                            )
+                        )
                     )}
                 </ul>
             </div>
